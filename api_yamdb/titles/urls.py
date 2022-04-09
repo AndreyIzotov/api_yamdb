273063c9b8c407cc6b24e92c5a53eb8a47adcc14
Андрей Изотов <andrej.izotoff@yandex.ru>
2022-04-09 19:39:01 +0300

    Приведите код в соответствие с PEP8

--- api_yamdb/titles/urls.py
+++ api_yamdb/titles/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from . import views
+from .views import GenreViewSet, TitleViewSet, СategorieViewSet
 
 router_v1 = SimpleRouter()
-router_v1.register('categories', views.СategorieViewSet, basename='categorie')
-router_v1.register('genres', views.GenreViewSet, basename='genre')
-router_v1.register('titles', views.TitleViewSet, basename='title')
+router_v1.register('categories', СategorieViewSet, basename='categorie')
+router_v1.register('genres', GenreViewSet, basename='genre')
+router_v1.register('titles', TitleViewSet, basename='title')
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
 ]
