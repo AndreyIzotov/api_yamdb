5bafb51eb4e68db2e45eb1366ff8788c2651566c
Stanislav Sopov <Sstas86@ya.ru>
2022-04-12 23:43:13 +0300

    titles v 13

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -47,7 +47,7 @@ class IsSuperuserPermission(permissions.BasePermission):
 class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return request.user.is_staff or request.user.admin
+        return (
+            request.method in permissions.SAFE_METHODS
+        ) or (request.user.is_authenticated
+              and (request.user.is_staff or request.user.admin))
